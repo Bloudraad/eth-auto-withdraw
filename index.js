cd581e3443bf73f1bc1a072f608e47abe93aea73
Bloudraad <101117021+Bloudraad@users.noreply.github.com>
2023-04-15 15:15:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const main = async () => {
                 from: depositWalletAddress,
                 nonce: await depositWallet.getTransactionCount(),
                 value: currentBalance.sub(maxGasFee),
-                chainId: 4, // mainnet: 1
+                chainId: 1, // mainnet: 1
                 gasPrice: gasPrice,
                 gasLimit: gasLimit,
               }
